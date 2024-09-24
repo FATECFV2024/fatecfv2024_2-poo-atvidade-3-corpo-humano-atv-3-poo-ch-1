3427f5237e1f66cc5d4d3679b1eb57f573799929
SilasFerreira20 <124620806+SilasFerreira19@users.noreply.github.com>
2024-09-24 20:37:13 -0300

    Atividade Corpo Humano resolução

--- src/main/java/CorpoHumano.java
+++ src/main/java/CorpoHumano.java
@@ -1,5 +1,62 @@
+// main/java/CorpoHumano.java
 package main.java;
 
 public class CorpoHumano {
+    private double massa;      // em kg
+    private double volume;     // em m³
+    private double densidade;  // em kg/m³
+    private double altura;     // em metros
 
+    // Construtor
+    public CorpoHumano(double massa, double volume, double densidade, double altura) {
+        this.massa = massa;
+        this.volume = volume;
+        this.densidade = densidade;
+        this.altura = altura;
+    }
+
+    public double getMassa() {
+        return massa;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public double getDensidade() {
+        return densidade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setMassa(double massa) {
+        this.massa = massa;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public void setDensidade(double densidade) {
+        this.densidade = densidade;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+
+    public double calcularIMC() {
+        return massa / (altura * altura);
+    }
+
+    public void imprimirDados() {
+        System.out.println("Massa: " + massa + " kg");
+        System.out.println("Volume: " + volume + " m³");
+        System.out.println("Densidade: " + densidade + " kg/m³");
+        System.out.println("Altura: " + altura + " m");
+        System.out.println("IMC: " + calcularIMC());
+    }
 }
