3fbfa3fe7f478e3e17fe333aa45ec379863c9ecb
Júlio Sandoli <juliosandoli1104@gmail.com>
2024-09-24 12:07:46 -0300

    Classe Corpo Humano
    Adicionado atributos, getters, setters, construtor e metodo calcularImc a classe corpo humano

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,26 +4,28 @@ import java.util.Scanner;
 public class App {
         //Comentário
 
-    public static void funcao_teste (){
-        //Biblioteca para entrada de dados
-        Scanner ler = new Scanner(System.in); 
+    // public static void funcao_teste (){
+    //     //Biblioteca para entrada de dados
+    //     Scanner ler = new Scanner(System.in); 
 
-        //variável local a
-        String v2;
-        int v1;
+    //     //variável local a
+    //     String v2;
+    //     int v1;
 
-        System.out.println("Comando de Saida de Dados");
+    //     System.out.println("Comando de Saida de Dados");
         
-        System.out.printf("Informe uma palavra: ");
-        v2 = ler.next();
+    //     System.out.printf("Informe uma palavra: ");
+    //     v2 = ler.next();
 
-        System.out.printf("Informe um número: ");
-        v1 = ler.nextInt();
+    //     System.out.printf("Informe um número: ");
+    //     v1 = ler.nextInt();
 
-        System.out.printf(v2+" "+v1);
-    }
-    public static void main(String[] args) throws Exception {
-        funcao_teste();
+    //     System.out.printf(v2+" "+v1);
+    // }
+    public static void main(String[] args) {
+
+        CorpoHumano c1 = new CorpoHumano(70, 19, 170);
+        System.out.println(c1.calcularImc());
 
 
     }
--- src/main/java/CorpoHumano.java
+++ src/main/java/CorpoHumano.java
@@ -1,5 +1,84 @@
 package main.java;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.GenericSignatureFormatError;
+import java.lang.reflect.Method;
+
 public class CorpoHumano {
 
-}
+    private int idade;
+    private int altura;
+    private String sexo;
+    private int massa;
+    private int volume;
+    private int densidade;
+    private boolean isAdult;
+
+    //CONSTRUCTOR
+    public CorpoHumano(int peso, int idade, int altura){
+        this.massa = peso;
+        this.idade = idade;
+        this.altura = altura;
+        if(idade > 18){
+            isAdult = true;
+        } else {
+            isAdult = false;
+        }
+    }
+
+    public double calcularImc() {
+        double alturaEmMetros = altura / 100.0;
+        return massa / (alturaEmMetros * alturaEmMetros);
+    }
+    //GETTERS
+        public int getPeso() {
+            return this.idade;
+        }
+        
+        public int getIdade() {
+            return this.massa;
+        }
+
+        public int getAltura() {
+            return this.altura;
+        }
+
+        public int getVolume(){
+            return this.volume;
+        }
+
+        public boolean getIsAdult(){
+            return this.isAdult;
+        }
+
+        public String getSexo(){
+            return this.sexo;
+        }
+
+        public int getDensidade(){
+            return this.densidade;
+        }
+
+    // SETTERS
+        public void setPeso(int peso) {
+            this.massa = peso;
+        }
+
+        public void setAltura(int altura) {
+            this.altura = altura;
+        }
+
+        public void setIdade(int idade) {
+            this.idade = idade;
+        }
+
+        public void display() {
+            System.out.println("Peso: " + massa + " kg");
+            System.out.println("Idade: " + idade + " anos");
+            System.out.println("Altura: " + altura + " cm");
+            System.out.println("É adulto: " + isAdult);
+            System.out.printf("IMC: %.2f\n", calcularImc());
+        }
+    }
+
+
