ca9b35c8f42b23f52c9d3ddbed3c4930d6cca581
Diego Veras <166725346+VerasDiego@users.noreply.github.com>
2024-09-25 12:26:52 -0300

    Add files via upload

--- src/main/java/CorpoHumano.java
+++ src/main/java/CorpoHumano.java
@@ -1,5 +1,44 @@
-package main.java;
-
 public class CorpoHumano {
+    // Atributos privados
+    private float massa;
+    private float volume;
+    private float densidade;
 
+    // Construtor
+    public CorpoHumano(float massa, float volume) {
+        this.massa = massa;
+        this.volume = volume;
+        this.densidade = calcularDensidade();
+    }
+
+    // Métodos Getters
+    public float getMassa() {
+        return massa;
+    }
+
+    public float getVolume() {
+        return volume;
+    }
+
+    public float getDensidade() {
+        return densidade;
+    }
+
+    // Métodos Setters
+    public void setMassa(float massa) {
+        this.massa = massa;
+        this.densidade = calcularDensidade();
+    }
+
+    public void setVolume(float volume) {
+        this.volume = volume;
+        this.densidade = calcularDensidade();
+    }
+
+    // Método para calcular a densidade
+    private float calcularDensidade() {
+        return massa / volume;
+    }
+    
 }
+
