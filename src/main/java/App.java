fb286d782c4bea5230b5cba62778cae84b4a8ca7
Gabriel da Silva <gabriel.rodrigues106@fatec.sp.gov.br>
2024-09-25 10:11:10 -0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,30 +1,29 @@
 package main.java;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
-        //Comentário
-
-    public static void funcao_teste (){
-        //Biblioteca para entrada de dados
-        Scanner ler = new Scanner(System.in); 
-
-        //variável local a
-        String v2;
-        int v1;
-
-        System.out.println("Comando de Saida de Dados");
         
-        System.out.printf("Informe uma palavra: ");
-        v2 = ler.next();
-
-        System.out.printf("Informe um número: ");
-        v1 = ler.nextInt();
-
-        System.out.printf(v2+" "+v1);
-    }
     public static void main(String[] args) throws Exception {
-        funcao_teste();
+        Scanner escrita = new Scanner(System.in);
+        escrita.useLocale(Locale.US);
 
+        CorpoHumano corpo = new CorpoHumano(80,60,50,1.65);
+
+        System.out.println("O corpo tem o volume de "+corpo.getVolume()+", tem a massa de "+corpo.getMassa()+", tem a densidade de "+corpo.getDensidade()+" e por fim tem a altura de "+corpo.getAltura());
+
+        System.out.println("Quanto você pesa? ");
+        double peso = escrita.nextDouble();
+        corpo.setMassa(peso);
+        
+        System.out.println("Qual é a sua altura: ");
+        double altura = escrita.nextDouble();
+        corpo.setAltura(altura);
+
+        System.out.println("Seu IMC é "+corpo.calcularIMC());
+
+
+        escrita.close();
 
     }
 }
